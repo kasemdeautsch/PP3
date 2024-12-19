66ab1efff6882317e6b736c941ccf2b9ca44474b
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-12-19 17:41:12 +0000

    add function to print average of subject

--- run.py
+++ run.py
@@ -72,10 +72,15 @@ def calculate_average(data, worksheet):
     new_data = []
     for column in data:
         int_column = [int(num) for num in column]
+        # max_grade = max(int_column)
         average = sum(int_column)/len(int_column)
+        # print(max_grade)
         new_data.append(round(average))
-    print(f"Average value of {worksheet} calculated Successfully.\n")
+    # print(f"Average value of {worksheet} calculated Successfully.\n")
+    # calculate_max_grade(data, worksheet)
+
     return new_data
+    # print(f"Average as list: {new_data}")
 
     # math = SHEET.worksheet('math').get_all_values()
     # pprint(math[-1])
@@ -90,7 +95,24 @@ def get_subject_grades(worksheet):
     for ind in range(1, 6):
         column = subject_worksheet.col_values(ind)
         columns.append(column[1:])
+    # pprint(columns)
+    # calculate_average(columns, worksheet)
     return columns
+
+
+def print_average_result(data, worksheet):
+    """
+    Gets the grades of particular worksheet
+    """
+    result = {}
+    worksheet_heading = SHEET.worksheet(worksheet).get_all_values()[0]
+    pprint(worksheet_heading)
+    for ind in range(5):
+        result[worksheet_heading[ind]] = data[ind]
+        # pprint(columns)
+        # calculate_average(columns, worksheet)
+        # return columns
+    print(result)
 
 
 def main():
@@ -110,8 +132,14 @@ def main():
 
 print("\nStaring the program.")
 print("------------------")
-main()
+# main()
 
-
+subject_columns = get_subject_grades('math')
+# get_subject_grades('math')
+pprint(subject_columns)
+average_data = calculate_average(subject_columns, 'math')
+print(average_data)
+print_average_result(average_data, 'math')
+# res = print_average_result(average_data, 'math')
 # student = SHEET.worksheet('math')
 # data = student.get_all_values()
